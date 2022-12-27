5e01ab8fa10c07fa6927189468c93db8b7fe1d5d
blademaw <jackoli3@me.com>
2022-12-28 00:01:31 +0800

    Fix single-author, title RIS tags

--- getcitation.py
+++ getcitation.py
@@ -3,12 +3,18 @@ import bibtexparser as bt
 import sys, re, rispy
 
 def formatRis(c_dict):
+	# print(c_dict) # debugging
 	# format authors
-	filler = " and " if len(c_dict['authors']) == 2 else ", "
-	c_dict['author'] = filler.join([f"{i[1]} {i[0]}" for i in list(map(lambda s: s.split(', '), c_dict['authors']))])
+	if len(c_dict['authors']) == 1:
+		temp = c_dict['authors'][0].split(', ')
+		c_dict['author'] = f"{temp[1]} {temp[0]}"
+	else:
+		filler = " and " if len(c_dict['authors']) == 2 else ", "
+		c_dict['author'] = filler.join([f"{i[1]} {i[0]}" for i in list(map(lambda s: s.split(', '), c_dict['authors']))])
 
 	# semantic corrections
-	c_dict['title'], c_dict['journal'] = c_dict['primary_title'], c_dict['journal_name']
+	c_dict['title'] = c_dict.get('primary_title') if c_dict.get('primary_title') is not None else c_dict.get('title')
+	c_dict['journal'] = c_dict['journal_name']
 
 	return c_dict
 
