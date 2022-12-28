4505a887d99ae898de92c3b13d47e773404d2ee8
blademaw <jackoli3@me.com>
2022-12-28 13:29:16 +0800

    Add clipboard support

--- getcitation.py
+++ getcitation.py
@@ -7,6 +7,9 @@ import sys, os, rispy
 FILETYPE = Enum('FileType', 'bibtex ris')
 
 def formatRis(c_dict):
+	# retrieval function
+	retrieve = lambda k: c_dict.get(k,f"No {k.split('_')[0]}")
+
 	# format authors
 	if len(c_dict['authors']) == 1:
 		temp = c_dict['authors'][0].split(', ')
@@ -16,68 +19,90 @@ def formatRis(c_dict):
 		c_dict['author'] = filler.join([f"{i[1]} {i[0]}" for i in list(map(lambda s: s.split(', '), c_dict['authors']))])
 
 	# semantic corrections
-	c_dict['title'] = c_dict.get('primary_title') if c_dict.get('primary_title') is not None else c_dict.get('title')
-	c_dict['journal'] = c_dict['journal_name']
+	c_dict['title'] = c_dict.get('primary_title') if c_dict.get('primary_title') is not None else retrieve('title')
+	c_dict['journal'] = retrieve('journal_name')
 
 	return c_dict
+
 
 def getStringType(f_name, ext, s):
 	if any([symbol in s.lstrip(' \n').split('\n')[0] for symbol in ['@', '{']]):
 		return FILETYPE.bibtex
 	elif s.lstrip(' \n')[:2] == 'TY':
 		return FILETYPE.ris
+
+	if f_name is None:
+		raise ValueError(f"Cannot detect format of citation in clipboard.")
 	raise ValueError(f"Cannot read contents of file with extension '{ext}' — if .bib or .ris ensure file contents are correct.")
 
 
-def getFileData(f_name, str_type=None, f_type=None):
-	ext = f_name.split('.')[-1]
-	c_dict = None
+def loadStringData(f_name, f_type, s):
+	c_dict = None 
 
-	with open(f_name) as file:
-		match ext:
-			case 'bib':
-				c_dict = bt.load(file).entries[0]
-				f_type = FILETYPE.bibtex
-			case 'ris':
-				c_dict = formatRis(rispy.load(file)[0])
-				f_type = FILETYPE.ris
-			case _:
-				# assume string; switch on filetype
-				try:
-					s = file.read()
-				except OSError:
-					raise ValueError(f"Cannot read file {f_name}.")
-				f_type = getStringType(f_name, ext, s)
-
-				match f_type:
-					# if string passed correctly, read file accordingly
-					case FILETYPE.bibtex:
-						c_dict = bt.loads(s).entries[0]
-					case FILETYPE.ris:
-						c_dict = formatRis(rispy.loads(s)[0])
-					case _:
-						raise ValueError(f"Type of contents in {f_name} not recognized.")
-
-				# raise ValueError(f"Extension {ext} not recognized.")
+	match f_type:
+		# if string passed correctly, read file accordingly
+		case FILETYPE.bibtex:
+			c_dict = bt.loads(s).entries[0]
+		case FILETYPE.ris:
+			c_dict = formatRis(rispy.loads(s)[0])
+		case _:
+			raise ValueError(f"Type of contents in {f_name} not recognized.")
 
 	return c_dict
 
-def parseCitation(f_name, str_type=None):
-	assert os.path.exists(f_name), f"Path {f_name} to file does not exist."
+
+def getFileData(f_name, str_type=None, f_type=None, s=None):
+	c_dict = None
+
+	if f_name is None:
+		f_type = getStringType(None, None, s)
+		c_dict = loadStringData(None, f_type, s)
+	else:
+		ext = f_name.split('.')[-1]
+
+		with open(f_name) as file:
+			match ext:
+				case 'bib':
+					c_dict = bt.load(file).entries[0]
+					f_type = FILETYPE.bibtex
+				case 'ris':
+					c_dict = formatRis(rispy.load(file)[0])
+					f_type = FILETYPE.ris
+				case _:
+					# assume string; switch on filetype
+					try:
+						s = file.read() if s is None else s
+					except OSError:
+						raise ValueError(f"Cannot read file {f_name}.")
+					f_type = getStringType(f_name, ext, s)
+					c_dict = loadStringData(f_name, f_type, s)
+
+	return c_dict
+
+
+def parseCitation(f_name, str_type=None, s=None):
+	if f_name is not None: assert os.path.exists(f_name), f"Path {f_name} to file does not exist."
 
 	# obtain file data
-	c_dict = getFileData(f_name, str_type)
+	c_dict = getFileData(f_name, str_type=str_type, s=s)
 	assert c_dict is not None, "Could not obtain data from file."
 
 	# format output string
 	retrieve = lambda k: c_dict.get(k,f'No {k}')
 	out_s = f"{retrieve('author')} ({retrieve('year')}). {retrieve('title')}. {retrieve('journal')}, {retrieve('doi')}"
 
-	# TODO: clean spacing, punctuation, random characters
+	# TODO: add cleaning for spacing, punctuation, random characters (encodings gone wrong)
 	return out_s
+
 
 if __name__ == '__main__':
 	if len(sys.argv) > 2:
+		# text format has been specified
 		print(parseCitation(sys.argv[1], sys.argv[2]))
+	elif len(sys.argv) == 1:
+		# assume citation is in clipboard
+		import pyperclip
+		print(parseCitation(None, s=pyperclip.paste()))
 	else:
+		# assume first argument is file path
 		print(parseCitation(sys.argv[1]))
